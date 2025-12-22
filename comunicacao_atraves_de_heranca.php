642ff2972ea6599bfe0fff6cac7bae6da93bbb03
Robstaine N. S. Macedo <robstaine@gmail.com>
2025-12-22 14:00:56 -0300

    corrigindo_codigo_e_criando_comentarios_para_melhor_compreencao

--- comunicacao_atraves_de_heranca.php
+++ comunicacao_atraves_de_heranca.php
@@ -1,56 +1,43 @@
 <?php
 
-abstract class Animal {
-    abstract public function emitirSom();
+abstract class Animal {     // classe abstrata serve como modelo para todos os animais
+    abstract public function emitirSom();       // método abstrato obriga as classes filhas a implementarem este método
 }
 
-class Cachorro extends Animal {
-    public $nome = "Cachorro";
+class Cachorro extends Animal {     // classe cachorro herda da classe abstrata animal
+    public $nome = "Cachorro";          // propriedade 'nome'
 
     public function __construct($nome) {
         $this->nome = $nome;
     }
 
-    public function emitirSom() {
+    public function emitirSom() {       // implementando método abstrato 'emitirSom'
         return "Au Au";
     }
 }
 
-class FilaBrasileiro extends Cachorro {
-    // public $nome = "Fila Brasileiro";
+class FilaBrasileiro extends Cachorro {     // estendendo a classe Cachorro
+    public $nome = "Fila Brasileiro";
     public $meses = 3;
 
-    public function emitirSom() {
-        if($this-> meses > 6) {
+    public function __construct($nome) {
+        parent::__construct($nome);
+    }
+
+    public function emitirSom() {       // 'sobrescrita' do método emitirSom
+
+        if($this-> meses > 6) {     // condição para emitir som especifico
             return "Ruf Ruf";
         } else {
-            return parent::emitirSom();
+            return parent::emitirSom();     // 'parent' permite ter acesso de classe especifica da classe pai
         }
         
     }
-}comunicacao_atraves_de_heranca.php
-
-class Gato extends Animal {
-    public $nome = "Gato";
-
-    public function emitirSom() {
-        return "Miau";
-    }
 }
 
-class Peixe extends Animal {
-    public $nome = "Peixe";
-
-    public function emitirSom() {
-        return null;
-    }
-}
-
-function fazerAnimalEmitirSom($animal) {
+function fazerAnimalEmitirSom($animal) {        // função recebe qualquer objeto Animal
     echo "<br> O animal " . $animal->nome . " faz: " . $animal->emitirSom() . "<br>";
 }
 
-$fila = new FilaBrasileiro("Dog");
+$fila = new FilaBrasileiro("Dog");      // 'instanciando' um objeto da classe FilaBrasileiro
 fazerAnimalEmitirSom($fila);
-
-?>
